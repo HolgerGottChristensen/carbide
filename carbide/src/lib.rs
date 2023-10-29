e98b44e6458c163b0ace8d0eef8645af503ae0a8
holgerchristensen <holger.gott@icloud.com>
2023-10-29 11:52:33 +0100

    Allow controls in wgpu examples, and make carbide_macro optional

--- carbide/src/lib.rs
+++ carbide/src/lib.rs
@@ -1,12 +1,14 @@
 
-#[macro_use]
-extern crate carbide_macro;
-
 pub use carbide_core::*;
+
+#[cfg(feature = "carbide_macro")]
+pub use carbide_macro::*;
+
+
 #[cfg(feature = "default")]
 pub use carbide_wgpu::*;
 
-#[cfg(feature = "controls")]
+#[cfg(feature = "carbide_controls")]
 pub mod controls {
     pub use carbide_controls::*;
 }
