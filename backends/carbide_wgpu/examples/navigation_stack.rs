a8707973a8deee7671006a6e19c7aaecbf4948ae
holgerchristensen <holger.gott@icloud.com>
2024-11-10 01:11:42 +0100

    Updated example to use root manager

--- backends/carbide_wgpu/examples/navigation_stack.rs
+++ backends/carbide_wgpu/examples/navigation_stack.rs
@@ -15,43 +15,43 @@ fn main() {
                 Text::new(format!("Index: {}", n)),
 
                 Button::new_primary("Push", move |ctx| {
-                    NavigationManager::get(ctx.env_stack, |manager| {
+                    NavigationManager::root(ctx.env_stack, |manager| {
                         manager.push(item(n + 1));
                     });
                 }).frame(80.0, 22.0),
 
                 Button::new_primary("Push 2", move |ctx| {
-                    NavigationManager::get(ctx.env_stack, |manager| {
+                    NavigationManager::root(ctx.env_stack, |manager| {
                         manager.extend(vec![item(n + 1), item(n + 2)])
                     })
                 }).frame(80.0, 22.0),
 
                 Button::new_primary("Pop", |ctx| {
-                    NavigationManager::get(ctx.env_stack, |manager| {
+                    NavigationManager::root(ctx.env_stack, |manager| {
                         manager.pop();
                     })
                 }).frame(80.0, 22.0),
 
                 Button::new_primary("Pop 2", |ctx| {
-                    NavigationManager::get(ctx.env_stack, |manager| {
+                    NavigationManager::root(ctx.env_stack, |manager| {
                         manager.pop_n(2);
                     })
                 }).frame(80.0, 22.0),
 
                 Button::new_primary("Pop all", |ctx| {
-                    NavigationManager::get(ctx.env_stack, |manager| {
+                    NavigationManager::root(ctx.env_stack, |manager| {
                         manager.pop_all();
                     })
                 }).frame(80.0, 22.0),
 
                 Button::new_primary("Replace", move |ctx| {
-                    NavigationManager::get(ctx.env_stack, |manager| {
+                    NavigationManager::root(ctx.env_stack, |manager| {
                         manager.replace(item(n));
                     })
                 }).frame(80.0, 22.0),
 
                 Button::new_primary("Replace all", move |ctx| {
-                    NavigationManager::get(ctx.env_stack, |manager| {
+                    NavigationManager::root(ctx.env_stack, |manager| {
                         manager.replace_all(item(0));
                     })
                 }).frame(80.0, 22.0),
@@ -63,7 +63,7 @@ fn main() {
     application.set_scene(Window::new(
         "NavigationStack - Carbide",
         Dimension::new(300.0, 300.0),
-        NavigationStack::new(item(0))
+        item(0)
     ).close_application_on_window_close());
 
     application.launch();
