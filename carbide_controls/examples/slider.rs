156ac3859c14453a871e82339c0731dbe3901f0e
holgerchristensen <holger.gott@icloud.com>
2025-02-07 23:01:18 +0100

    Indicator when the value is smaller or larger than the given range for the slider

--- carbide_controls/examples/slider.rs
+++ carbide_controls/examples/slider.rs
@@ -7,9 +7,9 @@ use carbide_wgpu::{Application, Window};
 use carbide_core::state::ReadStateExtNew;
 
 fn main() {
-    let progress = LocalState::new(60.0);
-    let progress2 = LocalState::new(5u32);
-    let progress3 = LocalState::new(30.0);
+    let state1 = LocalState::new(60.0);
+    let state2 = LocalState::new(5u32);
+    let state3 = LocalState::new(30.0);
 
     let mut application = Application::new()
         .with_asset_fonts();
@@ -18,28 +18,28 @@ fn main() {
         "Slider example - Carbide",
         Dimension::new(400.0, 600.0),
         VStack::new((
-            Text::new(progress.clone().map(|a| format!("Current value: {:.2}", a)))
+            Text::new(state1.map(|a| format!("Current value: {:.2}", a)))
                 .font_size(EnvironmentFontSize::Title2),
             Empty::new()
                 .frame(20.0, 40.0),
             Group::new((
                 Text::new("Slider 20.00 - 80.00"),
-                Slider::new(progress.clone(), 20.0, 80.0)
+                Slider::new(state1.clone(), 20.0, 80.0)
                     .padding(20.0),
                 Empty::new()
                     .frame(20.0, 20.0),
             )),
             Group::new((
                 Text::new("Slider 0.00 - 100.00"),
-                Slider::new(progress.clone(), 0.0, 100.0)
+                Slider::new(state1.clone(), 0.0, 100.0)
                     .padding(20.0)
                     .accent_color(EnvironmentColor::Green),
                 Empty::new()
                     .frame(20.0, 20.0),
             )),
             Group::new((
-                Text::new("Slider stepped 0.00 - 100.0"),
-                Slider::new(progress, 0.0, 100.0)
+                Text::new("Slider stepped 0.00 - 100.00"),
+                Slider::new(state1.clone(), 0.0, 100.0)
                     .step(Some(15.0))
                     .padding(20.0)
                     .accent_color(EnvironmentColor::Teal),
@@ -47,23 +47,15 @@ fn main() {
                     .frame(20.0, 20.0),
             )),
             Group::new((
-                Text::new(
-                    progress2
-                        .clone()
-                        .map(|a| format!("Slider integer: {:.2}", a)),
-                ),
-                Slider::new(progress2, 0, 25)
+                Text::new(state2.map(|a| format!("Slider integer: {:.2}", a))),
+                Slider::new(state2, 0, 25)
                     .accent_color(EnvironmentColor::Orange)
                     .padding(20.0),
                 Empty::new().frame(20.0, 20.0),
             )),
             Group::new((
-                Text::new(
-                    progress3
-                        .clone()
-                        .map(|a| format!("Slider disabled: {:.2}", a)),
-                ),
-                Slider::new(progress3, 20.0, 100.0)
+                Text::new("Slider disabled 20.00 - 80.00"),
+                Slider::new(state1, 20.0, 80.0)
                     .enabled(false)
                     .padding(20.0),
             ))
--- carbide_controls/src/slider/style/plain.rs
+++ carbide_controls/src/slider/style/plain.rs
@@ -1,8 +1,10 @@
+use carbide::closure;
 use carbide::color::TRANSPARENT;
 use carbide::environment::{EnvironmentColor, IntoColorReadState};
 use carbide::focus::Focus;
-use carbide::state::{AnyReadState, AnyState, Map1, Map2};
+use carbide::state::{AnyReadState, AnyState, Map1, Map2, ReadStateExtNew};
 use carbide::widget::{AnyWidget, Capsule, Circle, IfElse, RoundedRectangle, WidgetExt};
+use carbide::widget::canvas::{Canvas, CanvasContext};
 use crate::slider::style::SliderStyle;
 
 #[derive(Copy, Clone, Debug)]
@@ -11,7 +13,7 @@ pub struct PlainStyle;
 impl SliderStyle for PlainStyle {
     fn create_thumb(&self, focus: Box<dyn AnyReadState<T=Focus>>, enabled: Box<dyn AnyReadState<T=bool>>, percent: Box<dyn AnyReadState<T=f64>>, stepped: Box<dyn AnyReadState<T=bool>>) -> Box<dyn AnyWidget> {
 
-        let thumb_color = Map1::read_map(enabled, |enabled| {
+        let thumb_color = Map1::read_map(enabled.clone(), |enabled| {
             if *enabled {
                 EnvironmentColor::DarkText
             } else {
@@ -19,9 +21,40 @@ impl SliderStyle for PlainStyle {
             }
         });
 
+        let thumb_overlay_color = Map1::read_map(enabled, |enabled| {
+            if *enabled {
+                EnvironmentColor::Gray
+            } else {
+                EnvironmentColor::Gray4
+            }
+        });
+
+        let canvas = Canvas::new(closure!(|ctx: &mut CanvasContext| {
+            ctx.circle(ctx.width() / 2.0, ctx.height() / 2.0, ctx.width());
+
+            ctx.set_fill_style(*#thumb_color);
+            ctx.fill();
+
+            if *#percent < 0.0 {
+                ctx.begin_path();
+                ctx.move_to(ctx.width() / 10.0 * 6.0, ctx.width() / 10.0 * 3.0);
+                ctx.line_to(ctx.width() / 10.0 * 4.0, ctx.width() / 10.0 * 5.0);
+                ctx.line_to(ctx.width() / 10.0 * 6.0, ctx.width() / 10.0 * 7.0);
+                ctx.set_stroke_style(*#thumb_overlay_color);
+                ctx.stroke();
+            } else if *#percent > 1.0 {
+                ctx.begin_path();
+                ctx.move_to(ctx.width() / 10.0 * 4.0, ctx.width() / 10.0 * 3.0);
+                ctx.line_to(ctx.width() / 10.0 * 6.0, ctx.width() / 10.0 * 5.0);
+                ctx.line_to(ctx.width() / 10.0 * 4.0, ctx.width() / 10.0 * 7.0);
+                ctx.set_stroke_style(*#thumb_overlay_color);
+                ctx.stroke();
+            }
+        }));
+
         IfElse::new(stepped)
             .when_true(RoundedRectangle::new(2.0).fill(thumb_color.clone()).frame(8.0, 15.0))
-            .when_false(Circle::new().fill(thumb_color).frame(15.0, 15.0))
+            .when_false(canvas.frame(15.0, 15.0))
             .boxed()
     }
 
