8117c097b70d6e89cefc54d58a148a953329b66f
holgerchristensen <holger.gott@icloud.com>
2025-11-16 01:34:14 +0100

    Updated a few cargo tomls

--- carbide/src/lib.rs
+++ carbide/src/lib.rs
@@ -7,12 +7,6 @@ pub use carbide_macro::*;
 #[cfg(feature = "default")]
 pub use carbide_wgpu::*;
 
-#[cfg(feature = "media")]
-pub use carbide_media::*;
-
-#[cfg(feature = "i18n")]
-pub use carbide_fluent::*;
-
 #[cfg(feature = "carbide_controls")]
 pub mod controls {
     pub use carbide_controls::*;
@@ -20,6 +14,26 @@ pub mod controls {
 
 #[cfg(feature = "carbide_3d")]
 pub use carbide_3d::*;
+
+#[cfg(feature = "carbide_chart")]
+pub mod chart {
+    pub use carbide_chart::*;
+}
+
+#[cfg(feature = "i18n")]
+pub mod i18n {
+    pub use carbide_fluent::*;
+}
+
+#[cfg(feature = "carbide_dialogs")]
+pub mod dialogs {
+    pub use carbide_dialogs::*;
+}
+
+#[cfg(feature = "carbide_media")]
+pub mod media {
+    pub use carbide_media::*;
+}
 
 pub fn init() {
     static INITIALIZED: AtomicBool = AtomicBool::new(false);
